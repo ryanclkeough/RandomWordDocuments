65da70acdab45f923d428e0f93e3d27aac6221a0
Ryan <ryankeough0311@users.noreply.github.com>
2024-01-28 23:06:00 -0600

    WIP

--- RandomWordDocuments.py
+++ RandomWordDocuments.py
@@ -16,10 +16,11 @@ def main():
     except OSError as error:
         print(f"Error creating folder: {error}")
 
-    randomFileAmount = random.randrange(15, 30)
+    randomFileAmount = random.randrange(10, 15)
     for fileNumber in range(randomFileAmount):
         fileName = datetime.datetime.now().strftime("%Y_%m_%d-%H_%M_%S.%f")
         document = docx.Document()
+        RandomMathDocument()
         randomAmountOfCharacters = random.randrange(1000, 10000)
         textForDocument = ''.join(random.choices(string.ascii_letters, k=randomAmountOfCharacters))
         document.add_paragraph(textForDocument)
@@ -32,6 +33,17 @@ def main():
         print(f"Folder '{newFolderName}' and its contents have been deleted successfully.")
     except OSError as error:
         print(f"Error deleting folder: {error}")
+
+def RandomMathDocument():
+    mathString = ""
+    symbols = ["+", "-", "*", "/", "%", "=", "<", ">", "<=", ">=", " ", "\n"]
+
+    for character in range(18000, 24000):
+        randomLetters = random.randrange(1, 3)
+        mathString = mathString + random.choices(string.ascii_letters, k=randomLetters)
+        mathString = mathString + random.choices(symbols, weights=[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 1])
+
+    return mathString
 
 if __name__ == '__main__':
   main()
